cec6ead883309a7a5e3c84c653d058fc82bbb42d
frogr <austindanielfrench@gmail.com>
2018-09-20 15:45:51 -0700

    finds the largest prime factor (which will always be the last element of the array)

--- problems 1-10/3_largest_prime_factor.js
+++ problems 1-10/3_largest_prime_factor.js
@@ -3,20 +3,26 @@
 // What is the largest prime factor of the number 600851475143 ?
 
 const primeFactors = n => {
+  origNumber = n;
+  const factors = [];
+
   while (n % 2 === 0) {
-    console.log('2');
+    factors.push(2);
     n = n / 2;
   }
   for (let i = 3; i < Math.sqrt(n); i += 2) {
     while (n % i === 0) {
-      console.log(i);
+      factors.push(i);
       n = n / i;
     }
   }
   if (n > 2) {
-    console.log(n);
+    factors.push(n);
   }
-  return n;
+  console.log(`ALL PRIME FACTORS OF ${origNumber}: ${factors}`);
+  answer = factors[factors.length - 1];
+  console.log('LARGEST PRIME FACTOR:', answer);
+  return answer;
 };
 
 primeFactors(600851475143);
