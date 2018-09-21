b851744ce01ac06785b3b4a6a6b01e7fba404f56
frogr <austindanielfrench@gmail.com>
2018-09-20 21:21:46 -0700

    this solves the problem using brute force.. not too pretty

--- problems 1-10/5_smallest_multiple.js
+++ problems 1-10/5_smallest_multiple.js
@@ -5,17 +5,44 @@
 // all of the numbers from 1 to 20?
 
 const smallestMultiple = () => {
-  let start = 2520;
-  for (let i = 1; i <= 10; i++) {
-    if (start % i == 0) {
-      console.log('true');
-      return true;
+  let value = 232783000; // i happen to know the answer is 232792560 but we can't actually iterate from 2520 to find that, this is the lowest number we can start from to use this 'brute force' approach
+  let array = [];
+  checkDivisible(value, array);
+};
+
+const checkDivisible = (value, array) => {
+  console.log(value);
+  for (let i = 1; i < 21; i++) {
+    if (value % i === 0) {
+      array.push(true);
     } else {
-      console.log('false');
+      array.push(false);
+    }
+  }
+  console.log(array);
+  if (checkEqual(array) === true) {
+    console.log(
+      'Smallest positive number evenly divisible by the range:',
+      value
+    );
+    return array;
+  } else {
+    value++;
+    array = [];
+    checkDivisible(value, array);
+  }
+  return;
+};
+
+const checkEqual = arr => {
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[0]) {
+      // console.log('false');
       return false;
     }
   }
-  return;
+  // console.log('true');
+  return true;
 };
 
 smallestMultiple();
