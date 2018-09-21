133e81e0b409a040bd7411af386d35f72a1e8142
frogr <austindanielfrench@gmail.com>
2018-09-21 03:12:23 -0700

    cleaned up comments

--- problems 1-10/5_smallest_multiple.js
+++ problems 1-10/5_smallest_multiple.js
@@ -4,14 +4,16 @@
 // What is the smallest positive number that is evenly divisible by
 // all of the numbers from 1 to 20?
 
+// this is a VERY brute force method. i happen to know the answer is 232792560 but we can't actually iterate from 2520 to find that, this `value` is the lowest number we can start from to use this 'brute force' approach. it searches through about 200,000 numbers. node can't handle much more than that, it seems.
+
 const main = () => {
-  let value = 232595000; // i happen to know the answer is 232792560 but we can't actually iterate from 2520 to find that, this is the lowest number we can start from to use this 'brute force' approach
-  let array = [];
-  return checkDivisible(value, array);
+  let value = 232595000; // initial search value
+  return checkDivisible(value);
 };
 
-const checkDivisible = (value, array) => {
+const checkDivisible = value => {
   console.log(value);
+  let array = []; // instantiate a new array instead of using the old one
   for (let i = 1; i < 21; i++) {
     if (value % i === 0) {
       array.push(true);
@@ -29,11 +31,10 @@ const search = (value, array) => {
       'Smallest positive number evenly divisible by the range:',
       value
     );
-    return array;
+    return array; // win
   } else {
     value += 20; // increase our value by the upper boundary of our search range before the recursive call
-    array = []; // empty the array so that we can restart the function with a new array
-    return checkDivisible(value, array);
+    return checkDivisible(value); // lose, try again
   }
 };
 
